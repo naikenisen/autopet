e9e7e97bc8e73e2509d909271185e030af4b2936
Isen Naiken <isen.naiken@nexomedis.com>
2025-11-16 16:20:05 +0100

    nétoyage

--- train.sh
+++ train.sh
@@ -10,4 +10,4 @@ export PYTHONPATH=/work/imvia/in156281/venv/lib/python3.9/site-packages:$PYTHONP
 export MPLCONFIGDIR=/work/imvia/in156281/.cache/matplotlib
 export WANDB_CACHE_DIR=/work/imvia/in156281/.cache/wandb
 export WANDB_CONFIG_DIR=/work/imvia/in156281/.config/wandb
-python /beegfs/data/work/imvia/in156281/unet_gang/training.py
+python /beegfs/data/work/imvia/in156281/autopet/training.py
